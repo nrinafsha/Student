a777dd73dbcaa2a31a3f3d73ff88fede0eed0273
User <User@LAPTOP-QC69QVEV>
2023-10-16 00:57:29 +0800

    Nurrina Fassha Binti Iskandarsah 216583 13-3A, Stonor 3, Lorong Stonor, 50450, Kuala Lumpur, WP Kuala Lumpur

---
+++ Student.java/src/com/studyopedia/Student.java
@@ -0,0 +1,13 @@
+package com.studyopedia;
+
+public class Student {
+
+	public static void main(String[] args) {
+	
+			System.out.println("Nurrina Fassha Binti Iskandarsah");
+	        System.out.println("216583");
+	        System.out.println("13-3A, Stonor 3, Lorong Stonor, 50450, Kuala Lumpur, WP Kuala Lumpur");
+			
+	}
+
+}
